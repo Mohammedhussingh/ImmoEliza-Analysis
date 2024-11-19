2ff76d1976b95a1b50cbac2f6fe281085da7ef99
IVera96 <ilas.vera96@gmail.com>
2024-11-19 16:23:00 +0100

    # modified:   fun_expensive_locality.py  modified the fun_expensive_locality to obtain  fun_less_expensive_locality

--- fun_expensive_locality.py
+++ fun_expensive_locality.py
@@ -1,4 +1,6 @@
-def expensive_locality(df):
+import pandas as pd
+
+def less_expensive_locality(df):
 
     localities = df['Locality'].unique()
     df['Surface_total']=df['Living_Area']+df['Land_Surface']
@@ -13,11 +15,11 @@ def expensive_locality(df):
         average_price = locality_data['Price'].mean()
         median_price = locality_data['Price'].median()
         
-        valid_surface_data = locality_data[locality_data['Surface_total'] > 0]  # to evite that the result is inf because surface is missing
+        valid_surface_data = locality_data[locality_data['Surface_total'] > 0]  
         if not valid_surface_data.empty:
             price_per_square_meter = (valid_surface_data['Price'] / valid_surface_data['Surface_total']).mean()
         else:
-             price_per_square_meter = None  # Nessun calcolo se non ci sono superfici valide
+             price_per_square_meter = None  
 
       
         results.append({
@@ -26,8 +28,8 @@ def expensive_locality(df):
             'Median_Price': round(median_price, 2),
             'Avg_Price_per_SqM': round(price_per_square_meter, 2) if price_per_square_meter is not None else None
         })
-    return results
+        municipality_stats = pd.DataFrame(results)
+        municipality_stats = municipality_stats.sort_values(by='Average_Price', ascending=True)
 
-# Convert in a DataFrame for a understoodable output
-# municipality_stats = pd.DataFrame(results)
-# municipality_stats = municipality_stats.sort_values(by='Average_Price', ascending=False)
+    return municipality_stats
+
